cd7f603c95e722ee89e6ad5ac8211ebdb531f5b6
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-20 21:09:56 +0530

    Add files via upload

---
+++ Application-1.py
@@ -0,0 +1,160 @@
+##Accept 2 numbers from the user findout Sum, Subtraction, Multiplication, Division 2 numbers
+
+##Aprocach 1
+a=input("Enter a Number: ")
+b=input("Enter b Number: ")
+
+c=int(a)
+d=int(b)
+
+e=c+d
+
+print("Sum is: ",e)
+
+##Aproach 2
+a=input("Enter a number: ")
+b=input("Enter b number: ")
+
+c=int(a)+int(b)
+print("Sum is : ",c)
+
+##Aproach 3
+a=int(input("Enter a number: "))
+b=int(input("Enter b number: "))
+c=a+b
+
+print("Sum is: ",c)
+
+##Aproach 4
+a=int(input("Enter a number: "))
+b=int(input("Enter b number: "))
+
+print("Sum is: ", a+b)
+
+##Aproach 5
+print("Enter 2 Numbers: ")
+a,b=int(input()),int(input())
+c=a+b
+print("Sum is: ",c)
+
+##Subtraction
+
+##Aproach 1
+a = input("Enter a Number: ")
+b = input("Enter b Number: ")
+
+c = int(a)
+d = int(b)
+
+e = c - d
+
+print("Subtraction is:", e)
+
+##Aproach 2
+a = input("Enter a number: ")
+b = input("Enter b number: ")
+
+c = int(a) - int(b)
+print("Subtraction is:", c)
+
+##Aproach 3
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+c = a - b
+print("Subtraction is:", c)
+
+##Aproach 4
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+print("Subtraction is:", a - b)
+
+##Aproach 5
+print("Enter 2 Numbers: ")
+a, b = int(input()), int(input())
+
+c = a - b
+print("Subtraction is:", c)
+
+
+##Multiplication
+
+##Aproach 1
+a = input("Enter a Number: ")
+b = input("Enter b Number: ")
+
+c = int(a)
+d = int(b)
+
+e = c * d
+
+print("Multiplication is:", e)
+
+##Aproach 2
+a = input("Enter a number: ")
+b = input("Enter b number: ")
+
+c = int(a) * int(b)
+print("Multiplication is:", c)
+
+##Aproach 3
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+c = a * b
+print("Multiplication is:", c)
+
+##Aproach 4
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+print("Multiplication is:", a * b)
+
+##Aproach 5
+print("Enter 2 Numbers: ")
+a, b = int(input()), int(input())
+
+c = a * b
+print("Multiplication is:", c)
+
+##Division
+
+##Aproach 1
+a = input("Enter a Number: ")
+b = input("Enter b Number: ")
+
+c = int(a)
+d = int(b)
+
+e = c / d
+
+print("Division is:", e)
+
+##Aproach 2
+a = input("Enter a number: ")
+b = input("Enter b number: ")
+
+c = int(a) / int(b)
+print("Division is:", c)
+
+##Aproach 3
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+c = a / b
+print("Division is:", c)
+
+##Aproach 4
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+print("Division is:", a / b)
+
+##Aproach 5
+print("Enter 2 Numbers: ")
+a, b = int(input()), int(input())
+
+c = a / b
+print("Division is:", c)
+
