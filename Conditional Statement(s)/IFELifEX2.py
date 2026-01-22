a1bcd1ac0273772edcae8d4ff4899b3a250e5b55
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-22 20:35:30 +0530

    Add files via upload

---
+++ Conditional Statement(s)/IFELifEX2.py
@@ -0,0 +1,19 @@
+''' Accept Service number from the user print service name
+if sno is 100 print police
+if sno is 101 print fire
+if sno is 104 print medical
+if sno is 108 print emergency
+else print invalid service number
+'''
+sno = int(input("Enter Service Number: "))
+
+if sno == 100:
+    print("Police")
+elif sno == 101:
+    print("Fire")
+elif sno == 104:
+    print("Medical")
+elif sno == 108:
+    print("Emergency")
+else:
+    print("Invalid Service Number")
