e703231647e567fe7aea32f101f0a3a23208fd44
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-28 09:36:48 +0530

    Update FOR_2.py

--- Control Statement(s)/FOR_2.py
+++ Control Statement(s)/FOR_2.py
@@ -3,7 +3,8 @@ import time
 
 n=int(input("Enter a Number: "))
 
-for i in range(1,3):
+for i in range(1,2):
     time.sleep(.02)
     print(n,"Cube=",n*n*n)
+
 print("===============================================")
