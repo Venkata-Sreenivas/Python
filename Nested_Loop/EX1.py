d2a249b04a65271a7287c7e1fdb5cf27d71ecd63
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-28 18:40:40 +0530

    Add files via upload

---
+++ Nested_Loop/EX1.py
@@ -0,0 +1,12 @@
+'''To print sai 2 times'''
+
+import time
+for o in range(1,3):
+    for i in range(1,3):
+        time.sleep(.02)
+        print("Sai")
+    print()
+
+for i in range (1,4):
+    print("Sai")
+    print()
