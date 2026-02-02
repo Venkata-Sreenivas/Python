eb0fd5de554e7fa14b41411534f4663cff4c294e
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-02-02 20:33:22 +0530

    Add files via upload

---
+++ List_DataStructures/InsertDemo.py
@@ -0,0 +1,14 @@
+'''L.insert(position,item)'''
+
+lst=[10,20,30,40,50]
+
+print("List: ",lst)
+
+lst.insert(2,22)
+print("After insert: ",lst)
+
+lst.insert(-2,44)
+print("After inser: ",lst)
+
+lst.insert(9,99)
+print("After insert: ",lst)
