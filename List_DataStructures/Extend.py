0def421dc9eeb89f2d2cb7a05adf23f1291ea1c2
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-02-02 20:31:49 +0530

    Add files via upload

---
+++ List_DataStructures/Extend.py
@@ -0,0 +1,15 @@
+'''L.extend(iterable)'''
+
+a=[10,20,30]
+print("List a: ", a)
+
+b=[40,50,60]
+print("List b: ",b)
+
+c=a+b
+print("Result c: ",c)
+
+print("===============================")
+
+a.extend(b)
+print("Result a: ",a)
