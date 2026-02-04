c6f7dd072f59551aa90335be62a0f79358a50525
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-02-04 17:46:45 +0530

    Add files via upload

---
+++ Tuple_DataStructure/ReadingValuesFromTuples.py
@@ -0,0 +1,22 @@
+
+t=(10,20,30,40,50,60)
+
+print("Tuple is: ",t)
+
+#Indexing
+print("3rd item: ",t[2])
+print("3rd item: ",t[-4])
+print("Last but one: ",t[-2])
+print("Last but one: ",t[4])
+print("=============================")
+
+#Slicing
+print("First 3 objects: ",t[0:3:1])
+print("Last 3 objects: ",t[3:6:1])
+print("Last 3 objects in reverse: ",t[-1:-4:-1])
+
+#Unpacking
+print("")
+#Unpacking using slicing
+#while
+#for
